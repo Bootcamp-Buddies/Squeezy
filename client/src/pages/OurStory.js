89cc8b22fb5228ef9c74da92fda5908b9795279a
jscobie <112592660+jscobie@users.noreply.github.com>
2023-03-06 21:34:40 -0500

    Removing unused variables in OurStory

--- client/src/pages/OurStory.js
+++ client/src/pages/OurStory.js
@@ -1,34 +1,7 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
 import backgroundBlank from "../assets/squeezy-background-blank-opt.jpg";
 import Nav from '../components/Nav/index.js';
 
-function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+const OurStory = () => {
 
   return (
     <div className="container my-1">
@@ -60,4 +33,4 @@ function Login(props) {
   );
 }
 
-export default Login;
+export default OurStory;
