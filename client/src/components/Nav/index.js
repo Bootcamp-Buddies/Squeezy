38bbf78b161443f40d60d6f04535f5790addd37c
sgsetts <stephen.setterlind@gmail.com>
2023-03-06 20:50:53 -0500

    Fix nav to show for logged in users

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,19 +7,36 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+          <div class="container-fluid">
+            <Link className="navbar-brand" to="/">
+              <img
+                className="brand-logo"
+                src="./icons/squeezy-logo-white.png"
+                alt="Squeezy Logo"
+              />
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
             </Link>
-          </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
+
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link class="nav-link" aria-current="page" to="/orderHistory">Order History</Link>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#scrollspyHeading1">Products</a>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" aria-current="page" to="/story">Our Story</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       );
     } else {
       return (
